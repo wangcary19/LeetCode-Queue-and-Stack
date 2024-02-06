75e743d1500ea3ea036981b83425e52a13164575
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-02-05 23:56:39 -0500

    Update Moving Average

--- Queue: First-in-first-out Data Structure/Moving Average.py
+++ Queue: First-in-first-out Data Structure/Moving Average.py
@@ -1,14 +1,13 @@
-# February 2nd, 2024
-class MovingAverage:
+# February 4th, 2024
+def __init__(self, size: int):
+    self.l = []
+    self.window = size
 
-    def __init__(self, size: int):
-        self.l = []
-        self.window = size
 
-    def next(self, val: int) -> float:
-        self.l.append(val)
+def next(self, val: int) -> float:
+    self.l.append(val)
 
-        if len(self.l) < self.window:
-            return float(sum(self.l) / len(self.l))
-        else:
-            return float(sum(self.l[len(self.l) - self.window:]) / self.window)
+    if len(self.l) < self.window:
+        return float(sum(self.l) / len(self.l))
+    else:
+        return float(sum(self.l[len(self.l) - self.window:]) / self.window)
