7997a979c7c37a0cb6949efaac3d02271fd0b3b0
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-02-11 22:00:27 -0500

    Update Perfect Squares.py

--- Queue and BFS/Perfect Squares.py
+++ Queue and BFS/Perfect Squares.py
@@ -1,30 +1,23 @@
-# February 8th, 2024
+# February 11th, 2024
 def numSquares(self, n: int) -> int:
-    psref = [x ** 2 for x in range(0, 101)]
+    psref = [x ** 2 for x in range(1, 101)]
     q = [n]
     depth = 0
-    current_depth_limit = 1
-    future_depth_limit = 0
 
-    if n in psref:
-        return 1
-    else:
-        while True:
-            for x in range(0, current_depth_limit):
-                # print(q)
-                # print("q[x] is: " + str(q[x]))
-                if q[x] == 0:
-                    return depth
-                else:
-                    for i in range(1, 101):  # add to q all diffs of perfect squares less than q
-                        if psref[i] > q[x]:
-                            break
-                        else:
-                            q.append(q[x] - psref[i])
-                            future_depth_limit += 1
-            # end-for
-            q = q[current_depth_limit:]
-            current_depth_limit = future_depth_limit
-            future_depth_limit = 0
-            # print("new c_d_l is: " + str(current_depth_limit))
-            depth += 1
+    while len(q) > 0:
+
+        next_depth = set()
+
+        for x in range(0, len(q)):  # adds next depth of tree
+            if q[x] in psref:
+                return depth + 1
+            else:
+                for y in psref:
+                    if y > q[x]:
+                        break
+                    else:
+                        next_depth.add(q[x] - y)
+
+        q.clear()
+        q = list(next_depth)
+        depth += 1
