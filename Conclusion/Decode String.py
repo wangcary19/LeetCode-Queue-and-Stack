ab633dea567209251357b334a9223e35b2d06c42
Cary Wang <56293984+wangcary19@users.noreply.github.com>
2024-03-07 16:08:46 -0500

    Create Conclusion package

---
+++ Conclusion/Decode String.py
@@ -0,0 +1,33 @@
+num_stack = [3]
+letters_stack = []
+repeat = 0
+num_cache = ""
+word_cache = ""
+output = ""
+
+# can you do this recursively???
+
+"""
+for x in s:
+    if x.isnumeric():
+        num_cache += x
+    elif x.isalpha():
+        word_cache += x
+    elif x == "[":
+        num_stack.append(num_cache)
+        num_cache = ""
+        if word_cache != "":
+            letters_stack.append(word_cache)
+            word_cache = ""
+        else:
+            continue
+    else: # x == "]"
+        if word_cache != "":
+            output += word_cache * int(num_stack.pop(-1))
+            word_cache = ""
+        else:
+            output = (letters_stack.pop(-1) + output) * int(num_stack.pop(-1))
+#end-for
+
+return output + word_cache
+"""D
